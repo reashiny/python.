418816ddb49c904e7c9ee6bddb089089099ac175
reashiny <32952485+reashiny@users.noreply.github.com>
2017-10-21 11:04:20 +0530

    Add files via upload

--- python_player/variable.py
+++ python_player/variable.py
@@ -1,1 +1,28 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse037
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse037 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
 
+def main():
+    a=raw_input()
+    b=10
+    c=60
+    d=90
+    f=50
+    print(a)
+    print(int(a)+b)
+    print(str(a)+'1')
+    print(b**c)
+    print(d<>c)
+    print(d<>a)
+    print(c==d)
+    print(a,b,c,d,f)
+
+if __name__ == '__main__':
+    main()
