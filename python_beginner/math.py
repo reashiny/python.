9ea34d6d6ad57cb14062541e1f055b259ce22a1e
reashiny <32952485+reashiny@users.noreply.github.com>
2017-10-21 10:03:38 +0530

    Add files via upload

---
+++ python_beginner/math.py
@@ -0,0 +1,28 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse037
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16cse037 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    a=10
+    b=2
+    d=2
+    c=[10,20]
+    print(a+b)
+    print(a-b)
+    print(a**b)
+    print(a%b)
+    print(a==b)
+    print(id(a))
+    print(id(b))
+    print(id(d))
+    print(a<>d)
+    print(a in c)
+if __name__ == '__main__':
+    main()
