ebe60591f964c5eef6693c99660d1bf66332ff43
reashiny <32952485+reashiny@users.noreply.github.com>
2017-10-26 22:53:09 -0700

    Add files via upload

---
+++ python_beginner/check pos or neg num.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      mayil mass
+#
+# Created:     27/10/2017
+# Copyright:   (c) mayil mass 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    x=int(input("enter the integer"));
+    if x > 0:
+        print("positive number is:",x);
+    else:
+        print("negative number is:",x);
+
+
+if __name__ == '__main__':
+    main()
