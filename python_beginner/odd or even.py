fd48520aef644a149f58a7ab187c25cda645d07e
reashiny <32952485+reashiny@users.noreply.github.com>
2017-12-12 04:25:12 -0800

    Add files via upload

---
+++ python_beginner/odd or even.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Shiny
+#
+# Created:     11/12/2017
+# Copyright:   (c) Shiny 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+    a=int(input("enter the integer"))
+    print("check num is odd or even")
+    if(a%2==0):
+        print("even number is",a)
+    else:
+        print("odd number is",a)
+
