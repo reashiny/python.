fc9e03f2323133b443fa9b11a51df12a9a1fe4de
reashiny <32952485+reashiny@users.noreply.github.com>
2018-02-27 16:17:58 +0530

    Add files via upload

---
+++ python_beginner/vowel.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Name:        module5
+# Purpose:
+#
+# Author:      16CSE024
+#
+# Created:     27/02/2018
+# Copyright:   (c) 16CSE024 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+a=raw_input("enter an alphabet")
+if a in ('a','e','i','o','u'):
+    print ("%s is a vowel."%a)
+else:
+    print (a,"is an consonant")
+if a in ('A','E','I','O','U'):
+    print (a,"is an vowel")
+else:
+    print (a,"is an consonant")
