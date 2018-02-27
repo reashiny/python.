0cfbb18626b1dfe0e1cece673cf296588aa84c4c
reashiny <32952485+reashiny@users.noreply.github.com>
2018-02-27 16:27:15 +0530

    Update vowel.py

--- python_beginner/vowel.py
+++ python_beginner/vowel.py
@@ -7,7 +7,7 @@
 # Created:     27/02/2018
 # Copyright:   (c) 16CSE024 2018
 # Licence:     <your licence>
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 def main():
     pass
 
@@ -15,10 +15,6 @@ if __name__ == '__main__':
     main()
 a=raw_input("enter an alphabet")
 if (a=='a'or a=='e' or a=='i'or a=='o' or a=='u'):
-    print ("%s is a vowel."%a)
-else:
-    print (a,"is an consonant")
-if  (a=='A'or a=='E' or a=='I' or a=='O' or a=='U'):
-    print (a,"is an vowel")
+    print (a,"is a vowel")
 else:
     print (a,"is an consonant")
